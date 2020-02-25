afc06c42576d323ffa3614975bf64e2d5a772bae
Wei Si <sway0422@gmail.com>
2020-02-24 21:59:48 -0800

    Init project

---
+++ GPSFaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPSFaker
+//
+//  Created by Wei Si on 5/30/18.
+//  Copyright © 2018 Wei Si. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
